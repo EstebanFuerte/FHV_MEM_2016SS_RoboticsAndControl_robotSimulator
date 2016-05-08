0c0914f6692697d9c50ea6da215660a79619d91a
Stefan Stark <andreas1>
2016-05-08 21:13:41 +0200

    upload M6
    Excersice M6 according to exercise a mfuntions irb4600

--- exercises_1_transformationsAndsSacialDescription.m
+++ exercises_1_transformationsAndsSacialDescription.m
@@ -6,4 +6,24 @@ clear all, close all, clc;
 % accomplishes these rotations in the given order.
 T_1 = roty(30);
 T_2 = rotx(45);
-T = T_1*T_2
+T_E1a = T_1*T_2
+
+
+% E1b 
+% A frame {B} is located as follows: Initially coincident with a frame 
+% {A} we rotate {B} about ZB  by 30 degrees and then we rotate the 
+% resulting frame about XB  by 45 degrees. Give the rotation matrix 
+% which will change the description of vectors from  BP to  AP.
+
+T_1 = rotz(30);
+T_2 = rotx(45);
+T = T_1*T_2;
+R_E1b = T(1:3,1:3)
+
+% E1c
+% A frame {B} is conincident with frame {A}. {B} is first rotated around 
+% the yA ?axis by 45°, then rotated around the zA ?axis by 15°, then 
+% translated by 5 in the yA ?direction  and then rotated around the xA 
+% ?axis by 30°. Calculate the resulting transformation matrix.
+T_1 = roty(45)
+
---
+++ irb4600_robot.m
@@ -0,0 +1,29 @@
+function robot = irb4600_robot() 
+% robot.dhp DH parameters, [n 6] Array
+% colums:   type    sign alpha a d theta 
+%           (1/2) (1/-1)
+%           1...rotational axis
+%           2...translational axis
+%
+% robot.eff: tool frame (in flange coordinates)
+% [4 4] Array
+%
+% robot.bas: base frame (in world coordinates)
+% [4 4] Array
+% 
+% ALL ANGLES IN DEG!!
+
+field1 = 'dhp'; valueDHP = [1,1,0,0,495,0;
+                            1,1,-90,175,0,-90;
+                            1,1,0,900,0,0;
+                            1,1,90,175,-960,0;
+                            1,1,-90,0,0,0;
+                            1,1,-90,0,135,180];
+                        
+field2 = 'eff'; valueEff = dh_trafo_craig(-90,0,135,180);
+field3 = 'bas'; valueBas = dh_trafo_craig(0,0,495,0);
+
+robot = struct(field1,valueDHP, field2, valueEff, field3, valueBas);
+
+end
+
