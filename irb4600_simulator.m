b4ecd9fc1518043b0f5fea4368ecc8bb2e8642d9
Stefan Stark <andreas1>
2016-05-26 22:13:00 +0200

    implementation of cell array
    open calculate q correct?

--- irb4600_simulator.m
+++ irb4600_simulator.m
@@ -6,7 +6,7 @@ function irb4600_simulator()
 %   Detailed explanation goes here
 
 %% Aufräumen
-%clc, clear all, close all;
+clc, clear all, close all;
 
 %% Roboter laden
 robot = irb4600_robot;
@@ -14,42 +14,39 @@ robot = irb4600_robot;
 %% Angabe für die Simulaion
 ks_length = 100;
 
+% Angabe aus exercises a mfunction irb4600 - M13
 t_ipo=0.1;
 amax=1500;
-vc=500;
-pose=2;
-erase=1;
+vc=1000;
+pose=1;
 
-e(1)=[1040,0,1480,0,95,0,0];
-e(2)=[500,-1000,1000,0,180,0,2*vc];
-e(3)=[500,-1000,500,0,180,0,vc/2];
-e(4)=[500,-1000,1000,0,180,0,vc];
-e(5)=[500,-1100,1000,-90,90,0,vc];
-e(6)=[500,-1500,1000,-90,90,0,vc/2];
+% Euler - Coordinates = (x,y,z,alpha,beta,gamma,?)
+e{1}=[1000,-1000,1000,0,45,0,0];
+e{2}=[500,0,200,0,90,0,vc];
+e{3}=[-1000,1000,1000,0,135,0,vc];
+e{4}=[1000,0,1500,0,135,0,vc];
+e{5}=[-1000,-1000,1000,0,135,0,vc];
+e{6}=[1000,-1000,1000,0,45,0,vc];
 
-%% Trajektorie
+%% create trajekt.
 for i=1:length(e)-1
-    [tx,ax]=create_lin_seg_list( e((i+1)*1:i*3,1), e((i+1)*1:i*3,1), vc, amax, t_ipo);
-    [ t, a, v, s ] = create_lin_intvec( tx, ax, t_ipo );
-    ec(i) = create_lin_path( e(i)(1:6), e(i+1)(1:6), s );
-    if i == 1,
-        ec_total = ec(i);
-    else
-        ec_total = [ec_total, ec(i)];
-    end;
+    [tx,ax] = create_lin_seg_list(e{i},e{i+1},vc,amax,t_ipo);
+    [t,a,v,s] = create_lin_intvec(tx,ax,t_ipo);
+    ec{i} = create_lin_path(e{i},e{i+1},s);
 end
-%% berechnet celly array q
-for i=1:1:length(ec_total),
-    tg(i) = xyzabc_2_t(ec_total(i)(1), ec_total(i)(2), ec_total(i)(3), ec_total(i)(4), ec_total(i)(5), ec_total(i)(6));
-    q(i) = irb4600_rk( tg(i), robot.bas, robot.eff , pose)
-    
-    if isnan(q(i)) ~= 0,
-        error('Robot can not move this path in the desired pose!');
-    end;
-end;
+
+celldisp(ec)
+ec{1}{1}(1)
+
+
+%% q berechnen
+tg = xyzabc_2_t(ec{1}{i}(1),ec{1}{1}(2),ec{1}{1}(3),ec{1}{1}(4),ec{1}{1}(5),ec{1}{1}(6))
+q= irb4600_rk(tg,robot.bas,robot.eff, pose)
+
+
 
 %% Roboter Zeichnen
-draw_robot_path(q,0.5,robot,ks_length,1 );
+%draw_robot_path(q,t_ipo,robot,ks_length,1 );
 
 
 end
